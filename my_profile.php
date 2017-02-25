181a7ac47944fae4e1b92ed9deb1ef469e5b004e
unknown <bachtiarmuhmanggala@gmail.com>
2017-02-25 21:39:45 +0700

    aku push my_profile oke

--- my_profile.php
+++ my_profile.php
@@ -15,21 +15,27 @@
 			<li>
 				<a <?php if($module == "pesanan") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=pesanan&action=list"; ?>">Pesanan</a>
 			</li>
-			<li>
-				<a <?php if($module == "kategori") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=kategori&action=list"; ?>">Kategori</a>
-			</li>
-			<li>
-				<a <?php if($module == "barang") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=barang&action=list"; ?>">Barang</a>
-			</li>
-			<li>
-				<a <?php if($module == "kota") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=kota&action=list"; ?>">Kota</a>
-			</li>
-			<li>
-				<a <?php if($module == "user") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=user&action=list"; ?>">User</a>
-			</li>
-			<li>
-				<a <?php if($module == "banner") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=banner&action=list"; ?>">Banner</a>
-			</li>
+				<?php
+					if($level_user == "superadmin"){
+				?>
+					<li>
+						<a <?php if($module == "kategori") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=kategori&action=list"; ?>">Kategori</a>
+					</li>
+					<li>
+						<a <?php if($module == "barang") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=barang&action=list"; ?>">Barang</a>
+					</li>
+					<li>
+						<a <?php if($module == "kota") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=kota&action=list"; ?>">Kota</a>
+					</li>
+					<li>
+						<a <?php if($module == "user") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=user&action=list"; ?>">User</a>
+					</li>
+					<li>
+						<a <?php if($module == "banner") { echo "class='active'"; } ?> href="<?php echo BASE_URL."index.php?page=my_profile&module=banner&action=list"; ?>">Banner</a>
+					</li>
+				<?php
+					}
+				?>
 		</ul>
 	</div>
 

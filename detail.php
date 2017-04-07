b0f28bc8b75f3df4d2e85f35c1b377534f43674d
atlitnico <atlitnickof@gmail.com>
2017-04-07 17:57:26 +0700

    aku edit style.css, detail.php

--- detail.php
+++ detail.php
@@ -18,10 +18,10 @@
 		echo "<div id='detail-barang'>
 					<h2>$row[nama_barang]</h2>
 					<div id='frame-gambar'>
-						<img src='".BASE_URL."image/barang/$row[gambar]' />
+						<img src='".BASE_URL."images/barang/$row[gambar]' />
 					</div>
 					<div id='frame-harga'>
-						<span>".rupiah($row['harga'])."</span
+						<span>".rupiah($row['harga'])."</span>
 						<a href='".BASE_URL."tambah_keranjang.php?id_barang=$row[id_barang]'>+ add to cart</a>
 					</div>	
 					<div id='keterangan'>		
